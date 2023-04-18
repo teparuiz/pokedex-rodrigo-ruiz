da02a8488148c148a9ed443a73d265630853e5f4
teparuiz <rodrigo.j.ruiz.us@gmail.com>
2023-04-17 18:50:07 -0600

    paginator

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -3,10 +3,10 @@ import React from "react";
 function Pagination(props) {
   const { totalPages, setCurrentPage, currentPage } = props;
   const paginator = () => {
-    let pages = [];
+    const pages = [];
 
-    // Páginas iniciales
-    for (let i = 1; i <= Math.min(3, totalPages); i++) {
+    // Generar botones para todas las páginas
+    for (let i = 1; i <= totalPages; i++) {
       pages.push(
         <button
           key={i}
@@ -20,32 +20,23 @@ function Pagination(props) {
       );
     }
 
-    // Separador si hay más de 3 páginas
-    if (totalPages > 3) {
-      pages.push(
-        <span key="separator1" className="btn btn-outline-secondary">
-          ...
-        </span>
-      );
-    }
+    // Calcular los índices de inicio y fin de ambos rangos
+    const range1Start = Math.max(currentPage - 1, 1);
+    const range1End = Math.min(currentPage + 2, totalPages);
+    const range2Start = Math.max(currentPage + 19, 1);
+    const range2End = Math.min(currentPage + 21, totalPages);
 
-    // Páginas restantes de 20 en 20
-    for (let i = 20; i <= Math.min(22, totalPages); i++) {
-      pages.push(
-        <button
-          key={i}
-          onClick={() => setCurrentPage(i)}
-          className={`btn btn-outline-secondary ${
-            currentPage === i ? "active" : ""
-          }`}
-        >
-          {i}
-        </button>
+    // Filtrar los botones que están dentro de los rangos
+    const filteredPages = pages.filter((page, index) => {
+      return (
+        (index + 1 >= range1Start && index + 1 <= range1End) ||
+        (index + 1 >= range2Start && index + 1 <= range2End)
       );
-    }
+    });
 
-    return <div>{pages}</div>;
+    return <div>{filteredPages}</div>;
   };
+
   return (
     <div>
       <div className="text-center">{paginator()}</div>
